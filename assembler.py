438d12449069632bd40c3c82c3e25e6dd0659bb1
adityakak27 <146877885+adityakak27@users.noreply.github.com>
2024-03-11 20:30:36 +0530

    Update assembler.py
    Changed a few functions, added comments in the code; adjusted for virtual halt error

--- assembler.py
+++ assembler.py
@@ -1,7 +1,10 @@
-import re
-from dictionaries import * #importing dictionaries from dictionaries.py to avoid cluttering of code
+import re  #importing re(regular expressions) module to help with splitting lines etc.
+from dictionaries import *  #importing dictionaries
 
-def binary(number, size = 12):
+
+
+
+def binary(number, size = 12): #default size set to 12; specified otherwise if necessary 
     if number < 0:
         number = 2 ** size + number
     binary_string = bin(number)[2:]
@@ -10,24 +13,25 @@ def binary(number, size = 12):
 
 
 
+
 def assemble_r_type(instruction, rd, rs1, rs2):
     opcode = opcode_dict[instruction]
     funct3 = functions_dict[instruction]
-    funct7 = "0000000"  # default
+    funct7 = "0000000" #default
     return funct7 + registers_Dictionary[rs2] + registers_Dictionary[rs1] + funct3 + registers_Dictionary[rd] + opcode
 
 def assemble_i_type(instruction, rd, rs1, imm):
-    opcode = opcode_dict[instruction]
-    funct3 = functions_dict[instruction]
-    imm_binary = binary(imm)
-    imm_binary = imm_binary[-12:]  #take the last 12 bits
-    return imm_binary + registers_Dictionary[rs1] + funct3 + registers_Dictionary[rd] + opcode
+    opcode = opcode_dict[instruction] 
+    funct3 = functions_dict[instruction] #getting opcode and function in binary from the respective dictionaries 
+    imm_binary = binary(imm) #changing int immediate to binary
+    imm_binary = imm_binary[-12:]  #reversing immediate
+    return imm_binary + registers_Dictionary[rs1] + funct3 + registers_Dictionary[rd] + opcode #final binary output for entire line
 
 def assemble_s_type(instruction, rs1, rs2, imm):
     opcode = opcode_dict[instruction]
     funct3 = functions_dict[instruction]
     imm_binary = binary(imm)
-    imm_binary = imm_binary[-12:]  #take the last 12 bits
+    imm_binary = imm_binary[-12:]  
     imm_11_5 = imm_binary[:7]
     imm_4_0 = imm_binary[7:]
     return imm_11_5 + registers_Dictionary[rs2] + registers_Dictionary[rs1] + funct3 + imm_4_0 + opcode
@@ -36,9 +40,9 @@ def assemble_b_type(instruction, rs1, rs2, imm):
     opcode = opcode_dict[instruction]
     funct3 = functions_dict[instruction]
     imm_binary = binary(imm, 13)
-    imm_binary = imm_binary[-13:]  #take the last 13 bits
+    imm_binary = imm_binary[-13:]  
     imm_12 = imm_binary[0]
-    imm_10_5 = imm_binary[1:7]
+    imm_10_5 = imm_binary[1:7]  #splitting the immediate
     imm_4_1 = imm_binary[7:11]
     imm_11 = imm_binary[11]
     return imm_12 + imm_10_5 + registers_Dictionary[rs2] + registers_Dictionary[rs1] + funct3 + imm_4_1 + imm_11 + opcode
@@ -59,12 +63,16 @@ def assemble_j_type(instruction, rd, imm):
     imm_19_12 = imm_binary[12:20]
     return imm_20 + imm_10_1 + imm_11 + imm_19_12 + registers_Dictionary[rd] + opcode
 
+
+
 def parse_instruction(line):
     line = line.strip()
-    tokens = [token for token in re.split(r'[,\s\(\)]+', line) if token]
-    instruction = tokens[0]
+    tokens = [token for token in re.split(r'[,\s\(\)]+', line) if token] #splitting the input line into 'tokens';
+    instruction = tokens[0] #above tokens are categorised into instruction and operands
     operands = tokens[1:]
     return instruction, operands
+
+
 
 
 
@@ -73,11 +81,10 @@ def assemble_instruction(instruction, operands):
     if instruction not in instructions_Dictionary:
         return None  # invalid instruction
 
-    # Check if all operands are valid registers or immediate values
+    #check if all operands are valid or immediate values are numbers (adjusted for negative values)
     for operand in operands:
         if operand not in registers_Dictionary and not (operand.isdigit() or (operand.startswith('-') and operand[1:].isdigit())):
-            return None
-  # invalid operand
+            return None # any one operand or imm is found to be invalid
 
     if instruction in ["add", "sub", "sll", "slt", "sltu", "xor", "srl", "or", "and",
                        "mul", "rst", "halt", "rvrs", "div", "rem"]:
@@ -100,81 +107,91 @@ def assemble_instruction(instruction, operands):
 
     elif instruction == "sw":
         if len(operands) != 3:
-            return None  # incorrect no of operands
+            return None 
         rs1, rs2, imm = operands
-        if imm.isdigit() or (imm.startswith('-') and imm[1:].isdigit()):  # Check if immediate value
+        if imm.isdigit() or (imm.startswith('-') and imm[1:].isdigit()):  
             imm = int(imm)
             if abs(imm) >= 2**12:
-                return None  # illegal imm value
+                return None 
         else:
-            return None  # immediate value must be an integer
+            return None  
         return assemble_s_type(instruction, rs1, rs2, imm)
 
     elif instruction in ["beq", "bne", "blt", "bge", "bltu", "bgeu"]:
         if len(operands) != 3:
-            return None  # incorrect no of operands
+            return None  
         rs1, rs2, imm = operands
-        if imm.isdigit() or (imm.startswith('-') and imm[1:].isdigit()):  # Check if immediate value
+        if imm.isdigit() or (imm.startswith('-') and imm[1:].isdigit()):  
             imm = int(imm)
             if abs(imm) >= 2**12:
-                return None  # illegal imm value
+                return None 
         else:
-            return None  # immediate value must be an integer
+            return None  
         return assemble_b_type(instruction, rs1, rs2, imm)
 
     elif instruction in ["lui", "auipc"]:
         if len(operands) != 2:
-            return None  # incorrect no of operands
+            return None  
         rd, imm = operands
-        if imm.isdigit() or (imm.startswith('-') and imm[1:].isdigit()):  # Check if immediate value
+        if imm.isdigit() or (imm.startswith('-') and imm[1:].isdigit()):  
             imm = int(imm)
             if abs(imm) >= 2**20:
-                return None  # illegal imm value
+                return None 
         else:
-            return None  # immediate value must be an integer
+            return None 
         return assemble_u_type(instruction, rd, imm)
 
     elif instruction == "jal":
         if len(operands) != 2:
-            return None  # incorrect no of operands
+            return None  
         rd, imm = operands
-        if imm.isdigit() or (imm.startswith('-') and imm[1:].isdigit()):  # Check if immediate value
+        if imm.isdigit() or (imm.startswith('-') and imm[1:].isdigit()):  
             imm = int(imm)
             if abs(imm) >= 2**20:
-                return None  # illegal imm value--
+                return None
         else:
-            return None  # immediate value must be an integer
+            return None 
         return assemble_j_type(instruction, rd, imm)
 
     else:
-        return None  # invalid instruction
+        return None   #instruction provided is invalid
+
+
 
 
 
 
 def assemble_program(input_file, output_file):
     binary_code = []
+    vHalt_found = False #initialise variable
 
     with open(input_file, 'r') as file:
         for line_number, line in enumerate(file, 1):
             line = line.strip()
-            if not line or line.startswith('#'):
+            if not line or line.startswith('#'): #ignore empty lines or comments
                 continue
 
             instruction, operands = parse_instruction(line)
             binary_instruction = assemble_instruction(instruction, operands)
             if binary_instruction is None:
-                print(f"Error: Invalid instruction '{instruction}' found on line {line_number}")
+                print(f"Error: Invalid instruction '{instruction}' found on line {line_number}.") #invalid instruction reminder
                 return
             binary_code.append(binary_instruction)
 
- #   if binary_code[-1] != opcode_dict["halt"]:
-  #      print("Error: Virtual Halt must be used as the last instruction")
- #       return
+            if instruction == "beq" and set(operands) == {"0", "zero"}:
+                vHalt_found = True #change value if virtual halt command is found; breaks out of execution after virtual halt command
+                break
+
+    if not vHalt_found:
+        print("Error: Virtual Halt Command not found or not the last command. Please recheck.") #error message to user;
+        return
 
     with open(output_file, 'w') as file:
         for instruction in binary_code:
             file.write(instruction + '\n')
+
+
+
 
 
 # driver code
